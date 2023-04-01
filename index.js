e405df6814bf6aadaee215bede96a6c82c9e4e9a
vivianna ramirez <vivrami2001@gmail.com>
2023-04-01 10:50:56 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+ const multiply= (num1)* (num2); 
+ let random = Math.ceil(Math.random() * 100);
+ let max = Math.max(1,19,20);
+let num3=16;
+let num4=6;
+let mod= num3 % num4;
+console.log(mod);
